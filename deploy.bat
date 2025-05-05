26b7cf715a2496c2eb63769602d2e2a40faa1338
Katsu <abrahemeheron@gmail.com>
2025-05-04 22:43:33 -0300

    Atualiza├º├úo autom├ítica - Sun 05/04/2025 22:43:33.62

--- deploy.bat
+++ deploy.bat
@@ -3,5 +3,5 @@ cd /d "%~dp0"
 git add .
 git commit -m "Atualização automática - %date% %time%"
 git push origin main
-echo Site atualizado com sucesso! Acesse: https://katsugg.netlify.app
+echo Site atualizado! Acesse: https://katsugg.netlify.app
 pause
